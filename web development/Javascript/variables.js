6bc3d78c9a353ea936780ef8634a06bc7d408fa9
Palak <malikpalak1332@gamil.com>
2024-10-14 20:37:24 +0530

    knowing about different type of vaiables in javascript

---
+++ web development/Javascript/variables.js
@@ -0,0 +1,15 @@
+console.log("Helloworld");
+const accountid=12345
+//const will make it constant,so we will not be able to change it 
+let accountemail="palak@google.com"
+var accountpass="12345"
+//let is more preferable to use because var does not recognize block scope"{}" and functional scope
+accountcity="rohtak"
+//accountid=2
+//console.log(accountid);
+accountemail="malik@google.com"
+accountpass=43567
+accountcity="gurgaon"
+let accountt;
+//console.table will show values in table
+console.table([accountemail,accountpass,accountcity,accountt])
