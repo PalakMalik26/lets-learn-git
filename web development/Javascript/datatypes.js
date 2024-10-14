c4eebe4725a9081bc4f4fc040590389ac9f8c075
Palak <malikpalak1332@gamil.com>
2024-10-14 20:39:09 +0530

    different types of variables in javascript

---
+++ web development/Javascript/datatypes.js
@@ -0,0 +1,15 @@
+"use strict";  //treat all js code as newer version"
+//alert(3+3)  //we are using nodejs not browser
+//number=> 2 to the power of 53
+//bigint=> for large values
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+
+//object
+console.log(typeof null);  //null type is object
+console.log(typeof undefined);
+
