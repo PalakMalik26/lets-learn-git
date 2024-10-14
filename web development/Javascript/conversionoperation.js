fc322c028e05a71ceddb95d3ff0138976d11ded5
Palak <malikpalak1332@gamil.com>
2024-10-14 20:40:00 +0530

    conversion of one datatype to another

---
+++ web development/Javascript/conversionoperation.js
@@ -0,0 +1,18 @@
+/*let score="33abc"
+console.log(typeof score);
+console.log(typeof(score));
+let numberconvert=Number(score)
+console.log(typeof numberconvert)
+console.log(numberconvert);
+*/
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+let isl=1
+let isll=Boolean(isl)
+console.log(isll)
+//1=true; 0=>false
+//""=>false
+// "hitesh"=>true
+
+//Number can be converted to string also.By using string 
