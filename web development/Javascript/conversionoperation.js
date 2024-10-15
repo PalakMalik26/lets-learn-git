fd3d38f18b1b2e40a8d323fd114cc2703a9e0853
Palak <malikpalak1332@gamil.com>
2024-10-15 23:12:08 +0530

    knowing about conversion of different operators

--- web development/Javascript/conversionoperation.js
+++ web development/Javascript/conversionoperation.js
@@ -8,11 +8,34 @@ console.log(numberconvert);
 //"33"=>33
 //"33abc"=>NaN
 //true=>1; false=>0
-let isl=1
-let isll=Boolean(isl)
-console.log(isll)
+// let isl=1
+// let isll=Boolean(isl)
+// console.log(isll)
 //1=true; 0=>false
 //""=>false
 // "hitesh"=>true
 
 //Number can be converted to string also.By using string 
+//Operations
+// let value=3
+// let negv=-value
+// console.log(negv)
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2^2);
+// console.log(2/2);
+// let str1="hello";
+// let str2="hii";
+// let str3=str1+" "+str2;
+// console.log(str3)
+// console.log("1"+2)
+// console.log(1+"2");
+// console.log("1"+2+2); //agr string first to sbko string ki tarah consider kiya jayega
+// console.log(1+2+"2"); 
+// console.log(+true); //should not use such complex codes
+// let num1,num2,num3
+// num1=num2=num3=2+2;
+ 
+
